092c89f947d466452f4aec59e97d4b4ed7014869
Zerviol <45986500+Zerviol@users.noreply.github.com>
2021-12-14 13:18:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let rsp = ['Rock', 'Paper', 'Scissors'];
 
 function computerPlay() {
-	
+	let rsp = ['Rock', 'Paper', 'Scissors'];
 	let aRandom = rsp[Math.floor(Math.random() * 3)];
 	console.log(aRandom);
 }
