79e5559a6becdf7d9c7dda322635fedc29b7f409
Zerviol <45986500+Zerviol@users.noreply.github.com>
2021-12-15 11:57:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 function computerPlay() {
-	let rsp = ['Rock', 'Paper', 'Scissors'];
-	let aRandom = rsp[Math.floor(Math.random() * 3)];
-	console.log(aRandom);
-}
+   let game = ['rock', 'paper', 'scissor'];
+   let computerSelection = game[Math.floor(Math.random() * 3)];
+   let playerSelection = game[Math.floor(Math.random() * 3)];
+     playRound(playerSelection, computerSelection);
+ }
+ 
+ function playRound(playerSelection, computerSelection) {
+ 
+     if (
+        playerSelection === 'rock' && computerSelection === 'scissor' ||
+        playerSelection === 'paper' && computerSelection === 'rock' ||
+        playerSelection === 'scissor' && computerSelection === 'paper'
+     )
+     { alert('You win! Winner') } 
+ 
+     if (
+        computerSelection === 'rock' && playerSelection === 'scissor' ||
+        computerSelection === 'paper' && playerSelection === 'rock' ||
+        computerSelection === 'scissor' && playerSelection === 'paper'
+     )
+     { alert('You lose! Computer wins') }
+ 
+ }
+ 
+ const playerSelection = "rock";
+ const computerSelection = computerPlay();
+ console.log(playRound(playerSelection, computerSelection));
